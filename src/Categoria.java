833bd17ffc39bf9f5fb7c0db4510afe392a7a0f5
Vitor4818 <vitorg.martins4818@gmail.com>
2024-09-21 22:11:56 -0300

    WIP: Add Classe Categoria

---
+++ src/Categoria.java
@@ -0,0 +1,35 @@
+public class Categoria {
+
+    private String nome;
+    private double limite;
+    private double saldo;
+
+
+    public Categoria(String nome, double limite) {
+        this.nome = nome;
+        this.limite = limite;
+        saldo = limite;
+    }
+
+    public boolean ultrapassouLimite(double valorGasto) {
+        return valorGasto > limite; // Retorna true se o valorGasto for maior que o limite
+    }
+
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
